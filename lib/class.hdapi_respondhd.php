161d42475f3cda046dc5b4816bb7925d19fb5896
David Oliver <david@doliver.co.uk>
2011-07-19 12:05:10 +0200

    Trying to override HandsetDetection function

--- lib/class.hdapi_respondhd.php
+++ lib/class.hdapi_respondhd.php
@@ -1,5 +1,28 @@
 <?php
 class HandsetDetection_RespondHD extends HandsetDetection {
 	var $software_token = '';
+	
+	function HandsetDetection() {
+		
+		//parent::__construct();
+		//parent::HandsetDetection();
+		
+		// Set config based on Symphony Preferences
+		$this->setEmail('');
+		$this->setSecret('');
+		$this->setSoftwareToken('');
+		$this->setMobileSite('');
+		$this->setSiteId('');
+		
+		$this->setUseProxy('');
+		$this->setProxyServer('');
+		$this->setProxyPort('');
+		$this->setProxyUser('');
+		$this->setProxyPass('');
+		
+		$this->setLocalDetection('');
+		$this->fastjson = function_exists('json_encode') && function_exists('json_decode');
+	}
+	
 }
 ?>
